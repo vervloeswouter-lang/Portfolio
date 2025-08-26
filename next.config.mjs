112374b0bb99f2091197b188adf54895e74eb319
vervloeswouter-lang <vervloeswouter@gmail.com>
2025-08-26 15:03:17 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
--  experimental: { typedRoutes: true }
-+  // experimental: { typedRoutes: true } // disabled
+  // experimental: { typedRoutes: true }, // disabled
 };
+
 export default nextConfig;
